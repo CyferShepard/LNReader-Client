26ba167d6896cd82ebbd36ed8786efdb1916d58c
CyferShepard <govenderthegan@gmail.com>
2025-06-13 01:38:54 +0200

    Refactor Android project structure and update configurations
    - Updated Flutter project metadata and Gradle properties for improved performance.
    - Removed obsolete Android build files and settings.
    - Enhanced main activity and build scripts for better compatibility with Flutter.
    - Added context extensions to main.dart for improved functionality.

--- android/app/src/main/kotlin/com/example/light_novel_reader_client/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/light_novel_reader_client/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.light_novel_reader_client
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:light_novel_reader_client/components/nav_bar.dart';
 import 'package:light_novel_reader_client/components/navigation.dart';
+import 'package:light_novel_reader_client/extensions/context_extensions.dart';
 import 'package:light_novel_reader_client/globals.dart';
 import 'package:light_novel_reader_client/pages/auth/login.dart';
 import 'package:light_novel_reader_client/pages/auth/register.dart';
@@ -71,8 +72,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Scaffold(
+    return Obx(() {
+      if (context.isMobile && !serverController.serverResponse.success && !authController.auth.isAuthenticated) {
+        return Scaffold(
+          body: SettingsPage(),
+        );
+      }
+
+      return Scaffold(
         body: MainNavigationBar(navItems: [
           if (serverController.serverResponse.success && !authController.auth.isAuthenticated)
             NavBarItem(
@@ -147,7 +154,7 @@ class _HomePageState extends State<HomePage> {
               },
             ),
         ]),
-      ),
-    );
+      );
+    });
   }
 }
